154acebd2720f35b940217d0cd0abf4f5620ea1d
mm10791 <madisonmatthews0202@gmail.com>
2023-07-25 12:45:04 -0400

    Max_Matthews

--- src/java2finalproject/GUIElements.java
+++ src/java2finalproject/GUIElements.java
@@ -19,6 +19,6 @@ public class GUIElements extends Application {
     public void start(Stage primaryStage) {
         //main branch test
         //final main test
-        //final Max branch test
+        //final Max branch test...
     }
 }
