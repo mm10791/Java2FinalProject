c4fb82ed8d0ef831188081dcccb25964c8b7dfb6
mm10791 <madisonmatthews0202@gmail.com>
2023-07-25 12:30:56 -0400

    new branch?

--- src/java2finalproject/GUIElements.java
+++ src/java2finalproject/GUIElements.java
@@ -19,5 +19,6 @@ public class GUIElements extends Application {
     public void start(Stage primaryStage) {
         //testing number three for vscode and new branch
         //test some more
+        //maybe this branch will work?
     }
 }
